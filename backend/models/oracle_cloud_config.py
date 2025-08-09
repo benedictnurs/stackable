d929f156afa770f96f3e7b188c7bffbb855538ca
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-08-08 21:00:08 -0400

    Added shape model

--- backend/models/oracle_cloud_config.py
+++ backend/models/oracle_cloud_config.py
@@ -1,7 +1,17 @@
+from typing import Literal
 from pydantic import BaseModel
 
 
+class FlexShape(BaseModel):
+    shape: Literal["VM.Standard3.Flex"] = "VM.Standard3.Flex"
+    ocpus: float
+    memory_gb: int
+
+
 class OCIVars(BaseModel):
+    flex_shape: FlexShape
+    ocpus: float
+    memory_gb: int
     tenancy_ocid: str
     user_ocid: str
     fingerprint: str
